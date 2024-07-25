753d9e5a9d8a610ea1e3c7212dfbc4a62f348c95
Eryk Wójcik <drumpixtv@gmail.com>
2024-07-25 21:49:17 +0200

    Initial Commit

---
+++ LightMeter/LightMeterApp.swift
@@ -0,0 +1,17 @@
+//
+//  LightMeterApp.swift
+//  LightMeter
+//
+//  Created by Eryk Wójcik on 25/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct LightMeterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
