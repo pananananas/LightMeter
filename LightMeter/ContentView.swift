9fb8d1a4d2591fc961be0f3b20a114241ee488a2
Eryk Wójcik <drumpixtv@gmail.com>
2024-07-26 13:32:38 +0200

    implemented custom camera view

--- LightMeter/ContentView.swift
+++ LightMeter/ContentView.swift
@@ -1,14 +1,38 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var capturedImage: UIImage? = nil
+    @State private var isCustomCameraViewPresented = false
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack {
+            if capturedImage != nil {
+                Image(uiImage: capturedImage!)
+                    .resizable()
+                    .scaledToFill()
+                    .ignoresSafeArea()
+            } else {
+                Color(UIColor.systemBackground )
+            }
+            VStack {
+                Spacer()
+                Button(action: {
+                    isCustomCameraViewPresented.toggle()
+                }, label: {
+                    Image(systemName: "camera.fill")
+                        .font(.largeTitle)
+                        .padding()
+                        .background(Color.black)
+                        .foregroundColor(.white)
+                        .clipShape(Circle())
+                    
+                }).padding(.bottom)
+                    .sheet(isPresented: $isCustomCameraViewPresented, content: {
+                        CustomCameraView(capturedImage: $capturedImage)
+                    })
+            }
         }
-        .padding()
     }
 }
 
